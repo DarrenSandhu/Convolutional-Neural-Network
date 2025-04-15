d2491255b2a9f7944278f2f5f13f2cfeb7a50098
Darren Sandhu <darrensandhu7@gmail.com>
2025-04-15 11:33:10 +0100

    Decrease batch size

--- conv_neural_network/torch_files/train_cnn.py
+++ conv_neural_network/torch_files/train_cnn.py
@@ -6,7 +6,6 @@ from cnn_torch import Convolutional_Neural_Network  # Import your custom class
 from architecture.conv_layer_list_torch import Convolutional_Layers_Torch
 from data_methods.get_cnn_training_data_torch import images, labels
 
-device = torch.device("gpu" if torch.cuda.is_available() else "cpu")
 device = torch.device('mps' if torch.backends.mps.is_available() else 'cpu')
 print("Device: ",device)
 torch.set_default_device(device)
@@ -17,11 +16,12 @@ print("Images Shape: ",images.shape)
 output_nodes = 2
 target = labels
 print("Target Shape: ",target.shape)
-batch_size = 128
+batch_size = 10
+strides = [1,1,1]
 #conv_layers = Convolutional_Layers_Torch(2, [[8,5], [16,3]], ['relu', 'relu'], images.shape, batch_size)
 # conv_layers = Convolutional_Layers_Torch(2, [[4,5], [8,3]], ['relu', 'relu'], images.shape, batch_size)
-conv_layers = Convolutional_Layers_Torch(3, [[16,5], [32,3], [64,3]], ['relu', 'relu', 'relu'], images.shape, batch_size)
-# conv_layers = Convolutional_Layers_Torch(4, [[16,5], [32,3], [64,3], [128,3]], ['relu', 'relu', 'relu', 'relu'], images.shape, batch_size)
+conv_layers = Convolutional_Layers_Torch(3, [[16,5], [32,3], [64,3]], ['relu', 'relu', 'relu'], images.shape, batch_size, strides=strides, weight_init=None)
+# conv_layers= Convolutional_Layers_Torch(4, [[16,5], [32,3], [64,3], [128,3]], ['relu', 'relu', 'relu', 'relu'], images.shape, batch_size)
 
 
 # Create the model
@@ -30,7 +30,10 @@ cnn = Convolutional_Neural_Network(input_nodes,
                                    conv_layers)
 
 # Shorten image list for faster training
-images = torch.cat((images[:100], images[1000:1100]), dim=0)
-target = torch.cat((target[:100], target[1000:1100]), dim=0)
+images = torch.cat((images[:100], images[14000:14100]), dim=0)
+target = torch.cat((target[:100], target[14000:14100]), dim=0)
+print("\n\n\n\n\n")
+
+print("Training Model...")
 cnn.train(images, target, 1000, 0.001, batch_size)
 cnn.save("cnn_model.pth")
